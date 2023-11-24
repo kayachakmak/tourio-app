42c13eb96660bbf5137e46e5a1e51ebcffc7f70e
Ron McCoy <rongoods@gmail.com>
2023-11-24 11:14:13 +0100

    complete read portion of app

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,23 +1,33 @@
-import { db_places } from "../../../../lib/db_places";
-import { db_comments } from "../../../../lib/db_comments";
+import dbConnect from "../../../../db/connect";
+import Place from "../../../../db/models/place";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { id } = request.query;
 
   if (!id) {
     return;
   }
 
-  const place = db_places.find((place) => place._id.$oid === id);
-  const comment = place?.comments;
-  const allCommentIds = comment?.map((comment) => comment.$oid) || [];
-  const comments = db_comments.filter((comment) =>
-    allCommentIds.includes(comment._id.$oid)
-  );
-
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
+  await dbConnect();
+  if (request.method === "GET") {
+    const place = await Place.findById(id).populate("comments");
+    console.log("the place", place);
+    if (!place) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+    response.status(200).json(place);
   }
 
-  response.status(200).json({ place: place, comments: comments });
+  //   const place = db_places.find((place) => place._id.$oid === id);
+  //   const comment = place?.comments;
+  //   const allCommentIds = comment?.map((comment) => comment.$oid) || [];
+  //   const comments = db_comments.filter((comment) =>
+  //     allCommentIds.includes(comment._id.$oid)
+  //   );
+
+  //   if (!place) {
+  //     return response.status(404).json({ status: "Not found" });
+  //   }
+
+  //   response.status(200).json({ place: place, comments: comments });
 }
